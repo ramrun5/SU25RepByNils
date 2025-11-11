ec925c19ebefff2af9cb68ae1d854d42c21b0b13
ramrun5 <nils.winell@elev.ga.lbs.se>
2025-11-11 11:29:06 +0100

    FINALLY I MADE THE ENEMY DIE AND GIVE YOU POINTS

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,32 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Enemy : MonoBehaviour
 {
+    PointManager points;
     public int health = 100;
+    public GameObject Player;
     public void TakeDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
             Die();
+            if (health <= 0)
+            {
+                points.playerPoints += 1;
+            }
         }
+
     }
     void Die()
     {
+        StartCoroutine(Death());
+    }
+    IEnumerator Death()
+    {
+            yield return new WaitForSeconds(0f);
         Destroy(gameObject);
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
     {
-        
+        points = FindAnyObjectByType<PointManager>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+    
--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -1,15 +1,15 @@
 using TMPro;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class PointManager : MonoBehaviour
 {
     [SerializeField] TMP_Text pointDisplay;
     [SerializeField] public int playerPoints = 0;
-    PointManager points;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
